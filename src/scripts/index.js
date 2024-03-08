80ae757a900fb2bf25de41a1d29b27f05397df6d
Emanuel Fortes <emanuelbf19@gmail.com>
2024-03-08 11:10:12 -0300

    projeto finalizado

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,56 +1,43 @@
-import { user } from './services/users.js';
-import { respositories } from './services/repositories.js';
+import { getUser } from './services/users.js';
+import { getRespositories } from './services/repositories.js';
+
+import { user } from './objects/users.js'
+import { screen } from './objects/screen.js';
 
 document.getElementById("btn-search").addEventListener("click", () => {
   const userName = document.getElementById("input-search").value;
-  getUserProfile(userName);
+  if(validateEmptyInput(userName)) return
+  getUserData(userName);
 });
+
 document.getElementById("input-search").addEventListener("keyup", (e) => {
   const userName = e.target.value;
   const key = e.which || e.keyCode;
   const isEnterKeyPressed = key === 13;
   if (isEnterKeyPressed) {
-    getUserProfile(userName);
+    if(validateEmptyInput(userName)) return
+    getUserData(userName);
   }
 });
 
-
-
-
-
-function getUserProfile(userName) {
-  user(userName).then((userData) => {
-    let userInfo = `<div class="info">
-                        <img src="${
-                          userData.avatar_url
-                        }" alt="Foto Do perfil do usuário"/>
-                           <div class="data">
-                            <h1>${
-                              userData.name ?? "Não possui nome cadastrado"
-                            }</h1>
-                            <p>${
-                              userData.bio ?? "Não possui bio cadastrada"
-                            }</p>
-                           </div>
-                        </div>`;
-    document.querySelector(".profile-data").innerHTML = userInfo;
-
-    getUserRepositories(userName);
-  });
+function validateEmptyInput(userName){
+  if(userName.length === 0 ){
+    alert('Preencha o campo com o nome do usuario do GitHub')
+    return true
+  }
 }
 
-function getUserRepositories(userName) {
-  respositories(userName).then((reposData) => {
-    let repositoriesItens = "";
+async function getUserData(userName) {
 
-    reposData.forEach((repo) => {
-      repositoriesItens += `<li><a hraf="${repo.html_url}">${repo.name}</a></li>`;
-    });
-    document.querySelector(
-      ".profile-data"
-    ).innerHTML += `<div class="repositories section"> 
-                        <h2>Repositório</h2>
-                        <ul>${repositoriesItens}</ul>
-                    </div>`;
-  });
-}
+    const userResponse = await getUser(userName)
+
+    if(userResponse.message === "Not Found"){
+      screen.renderNotFound()
+      return
+    }
+
+    const repositoriesResponse = await getRespositories(userName)
+    user.setInfo(userResponse)
+    user.setRpositories(repositoriesResponse)
+    screen.renderUser(user)
+ }
--- src/scripts/services/repositories.js
+++ src/scripts/services/repositories.js
@@ -1,10 +1,8 @@
 import { baseUrl, repositoriesQuantity } from "../variables.js";
 
-async function respositories(userName) {
-  const response = await fetch(
-    `${baseUrl}/${userName}/repos?per_page=${repositoriesQuantity}`
-  );
+async function getRespositories(userName) {
+  const response = await fetch(`${baseUrl}/${userName}/repos?per_page=${repositoriesQuantity}`);
   return await response.json();
 }
 
-export { respositories };
+export { getRespositories };
